1dd903a4ada90f526bb2b87632f38b4a89568481
WilsonParedes <wparedesp@miumg.edu.gt>
2021-08-31 22:40:34 -0600

    Archivo sin error

--- NuevoProSO/BitacoraP.h
+++ NuevoProSO/BitacoraP.h
@@ -15,52 +15,53 @@ private:
     string paternidad;
     string tiempoejecución;
 
-public: 
-   
+public:
+
     BitacoraP(string NoProceso, string TemaEjecucion, PrioridadProceso pp, string paternidad, string tiempoejecucion) {
         this->NoProceso = NoProceso;
         this->TemaEjecucion = TemaEjecucion;
-        this->pp = pp; 
+        this->pp = pp;
         this->paternidad = paternidad;
         this->tiempoejecución = tiempoejecucion;
     }
 
-      void setNoProceso(string NoProceso) {
-          this->NoProceso = NoProceso;
-      }
-      void setTemaEjecucion(string TemaEjecucion) {
-          this->TemaEjecucion = TemaEjecucion;
-      }
-      void setPrioridadProceso(PrioridadProceso pp) {
-          
-          this->pp = pp;
-      }
-      void setPaternidad(string paternidad) {
-          this->paternidad = paternidad;
-      }
-      void setTiempoEjecucion(string TemaEjecucion) {
-          this->tiempoejecución = tiempoejecución;
-      }
+    void setNoProceso(string NoProceso) {
+        this->NoProceso = NoProceso;
+    }
+    void setTemaEjecucion(string TemaEjecucion) {
+        this->TemaEjecucion = TemaEjecucion;
+    }
+    void setPrioridadProceso(PrioridadProceso pp) {
 
-      string getNoProceso() {
-          return NoProceso;
-      }
-      string getTemaEjecucion() {
-          return TemaEjecucion;
-      }
-      PrioridadProceso getPrioridadProceso() {
-          return pp;
-      }
-      string getPaternidad() {
-          return paternidad;
-      }
-      string setTiempoEjecucion() {
-          return tiempoejecución;
-      }
+        this->pp = pp;
+    }
+    void setPaternidad(string paternidad) {
+        this->paternidad = paternidad;
+    }
+    void setTiempoEjecucion(string TemaEjecucion) {
+        this->tiempoejecución = tiempoejecución;
+    }
 
-      string toString() {
-          return NoProceso +"\t\t" + " | " + TemaEjecucion + "\t\t" + " | " + paternidad + "\t\t" + " | " + tiempoejecución;
-      }
+    string getNoProceso() {
+        return NoProceso;
+    }
+    string getTemaEjecucion() {
+        return TemaEjecucion;
+    }
+    PrioridadProceso getPrioridadProceso() {
+        return pp;
+    }
+    string getPaternidad() {
+        return paternidad;
+    }
+    string setTiempoEjecucion() {
+        return tiempoejecución;
+    }
+
+    string toString() {
+        return NoProceso + "\t\t" + " | " + TemaEjecucion + "\t\t" + " | " + paternidad + "\t\t" + " | " + tiempoejecución;
+    }
+
 
 
 };
